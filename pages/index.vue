fc67e47b9ea615f9b896b9527e0a50dd4af1c71f
mykhailo <m.diakovych13@gmail.com>
2022-03-15 16:40:18 +0200

    initial

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <DesignerClothes />
+</template>
+
+<script>
+import DesignerClothes from '~/components/DesignerClothes'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    DesignerClothes,
+  },
+}
+</script>
