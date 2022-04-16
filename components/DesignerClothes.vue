00e230b19e40a25376cb784e39d0599cec7e9e46
mykhailo <m.diakovych13@gmail.com>
2022-04-16 21:25:00 +0300

    Add: share twitter

--- components/DesignerClothes.vue
+++ components/DesignerClothes.vue
@@ -52,12 +52,16 @@
           <b-button variant="danger" @click="onGetJSON">Get JSON</b-button>
         </b-form-group>
       </b-col>
+      <b-col cols="5" offset="1">
+        <a class="twitter-share-button" href="https://twitter.com/intent/tweet" data-size="large">Tweet</a>
+      </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
 import { fabric } from 'fabric'
+import getShareImage from '@jlengstorf/get-share-image';
 import {
   canvasItems,
   saveImage,
@@ -65,6 +69,16 @@ import {
   uploadImage,
 } from '~/utils/utils'
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '~/utils/constants'
+
+const socialImage = getShareImage({
+  title: 'Deploy a Node.js App to DigitalOcean with SSL',
+  tagline: '#cryptaine #nodejs',
+  cloudName: 'mykhailo',
+  imagePublicID: '/share.png',
+  titleFont: 'futura',
+  taglineFont: 'futura',
+  textColor: '232129',
+});
 
 export default {
   name: 'DesignerClothes',
@@ -83,6 +97,86 @@ export default {
     designColor: '#ffffff',
     CANVAS_WIDTH,
     CANVAS_HEIGHT,
+  }),
+  head: () => ({
+    title: 'Designer Clothes',
+    meta: [
+      {
+        hid: 'title',
+        property: 'title',
+        content: 'Clothes Design'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'description'
+      },
+      {
+        hid: 'image',
+        property: 'image',
+        content: socialImage
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://clothes-preview.vercel.app/',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Clothes Design'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'description'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: socialImage
+      },
+      {
+        hid: 'og:image:width',
+        property: 'og:image:width',
+        content: '200',
+      },
+      {
+        hid: 'og:image:height',
+        property: 'og:image:height',
+        content: '200',
+      },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'twitter:url',
+        name: 'twitter:url',
+        content: 'https://clothes-preview.vercel.app/',
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Clothes Design'
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: 'description'
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: socialImage
+      }
+    ]
   }),
   mounted() {
     const TShirtCanvas = this.$refs.TShirtCanvas
