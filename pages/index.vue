4c519684dcae101f005542356d8cae0cf58588aa
mykhailo <m.diakovych13@gmail.com>
2022-04-17 17:47:13 +0300

    Add: share twitter

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <b-form-group class='mb-2'>
       <ShareNetwork
         network='twitter'
-        url='https://res.cloudinary.com/dniafpygj/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1650203926/Screen_Shot_2022-04-17_at_4.57.34_PM_er0o3f.png'
+        url='https://res.cloudinary.com/dniafpygj/image/upload/v1650203926/Screen_Shot_2022-04-17_at_4.57.34_PM_er0o3f.png'
         title='Ttl'
         description='Desc'
         twitter-user='youyuxi'
@@ -15,23 +15,13 @@
     </b-form-group>
 
     <b-form-group class='mb-2'>
-      <a class='twitter-share-button' href='https://twitter.com/intent/tweet' data-size='large'>Tweet 1</a>
+      <a class='twitter-share-button' href='https://twitter.com/intent/tweet?url=https://res.cloudinary.com/dniafpygj/image/upload/v1650203926/Screen_Shot_2022-04-17_at_4.57.34_PM_er0o3f.png' data-size='large'>Tweet 1</a>
     </b-form-group>
     <b-form-group class='mb-2'>
       <a
-        href='https://twitter.com/intent/tweet?text=optional%20promo%20text%20http://example.com/foo.htm?bar=123&baz=456'
+        href='https://twitter.com/intent/tweet'
         target='_blank'>Tweet target="_blank"</a>
     </b-form-group>
-
-    <blockquote class='twitter-tweet'>
-      <p lang='en' dir='ltr'>Sunsets don&#39;t get much better than this one over <a
-        href='https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw'>@GrandTetonNPS</a>. <a
-        href='https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw'>#nature</a> <a
-        href='https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw'>#sunset</a> <a
-        href='http://t.co/YuKy2rcjyU'>pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior
-      (@Interior) <a href='https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw'>May 5, 2014</a>
-    </blockquote>
-    <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>
   </div>
 </template>
 
@@ -59,7 +49,7 @@ export default {
       {
         hid: 'image',
         property: 'image',
-        content: '/image.png'
+        content: '/image_1.png'
       },
       {
         hid: 'og:type',
@@ -84,7 +74,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: '/image.png'
+        content: '/image_1.png'
       },
       {
         hid: 'og:image:width',
@@ -119,7 +109,7 @@ export default {
       {
         hid: 'twitter:image',
         name: 'twitter:image',
-        content: '/image.png'
+        content: '/image_1.png'
       }
     ]
   })
